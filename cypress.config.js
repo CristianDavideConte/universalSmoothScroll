a6fe023ee4b28addaa22d199f0933d1515b37adb
Cristian Davide Conte <64142998+CristianDavideConte@users.noreply.github.com>
2022-06-09 10:35:00 +0200

    tests now uses cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  retries: 5,
+  env: {
+    preferredTimeout: 4000,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8080',
+  },
+}
